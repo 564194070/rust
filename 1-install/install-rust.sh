1e48344f6c807e2ff4c688cabf7ae0e63f3c6f9e
wentao zhou <Wentao.Zhou@metax-tech.com>
2025-01-16 03:07:18 +0000

    how to install rust

---
+++ 1-install/install-rust.sh
@@ -0,0 +1,3 @@
+export RUSTUP_UPDATE_ROOT=https://mirrors.tuna.tsinghua.edu.cn/rustup/rustup
+export RUSTUP_DIST_SERVER=https://mirrors.tuna.tsinghua.edu.cn/rustup
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
